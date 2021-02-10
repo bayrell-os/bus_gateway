4539e9b5186582df1414c54e627c7b5daf6d63a5
Ildar Bikmamatov <ildar@bayrell.org>
2021-02-10 18:41:15 +0000

    add arm64

--- build.sh
+++ build.sh
@@ -19,6 +19,10 @@ case "$1" in
 		docker build ./ -t bayrell/bus_gateway:$VERSION-amd64 --file Dockerfile --build-arg ARCH=-amd64
 	;;
 	
+	arm64v8)
+		docker build ./ -t bayrell/bus_gateway:$VERSION-arm64v8 --file Dockerfile --build-arg ARCH=-arm64v8
+	;;
+	
 	arm32v7)
 		docker build ./ -t bayrell/bus_gateway:$VERSION-arm32v7 --file Dockerfile --build-arg ARCH=-arm32v7
 	;;
@@ -27,22 +31,25 @@ case "$1" in
 		rm -rf ~/.docker/manifests/docker.io_bus_gateway-*
 		
 		docker push bayrell/bus_gateway:$VERSION-amd64
+		docker push bayrell/bus_gateway:$VERSION-arm64v8
 		docker push bayrell/bus_gateway:$VERSION-arm32v7
 		
 		docker manifest create --amend bayrell/bus_gateway:$VERSION \
 			bayrell/bus_gateway:$VERSION-amd64 \
+			bayrell/bus_gateway:$VERSION-arm64v8 \
 			bayrell/bus_gateway:$VERSION-arm32v7
 		docker manifest push --purge bayrell/bus_gateway:$VERSION
 	;;
 	
 	all)
 		$0 amd64
+		$0 arm64v8
 		$0 arm32v7
 		$0 manifest
 	;;
 	
 	*)
-		echo "Usage: $0 {amd64|arm32v7|manifest|all|test}"
+		echo "Usage: $0 {amd64|arm64v8|arm32v7|manifest|all|test}"
 		RETVAL=1
 
 esac
